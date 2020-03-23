e7a516e8d398767bdf15abe8469be0797b79082c
SibirBear <test@test.com>
2020-03-24 00:20:09 +0700

    add point check into class Field

--- src/com/sibirbear/study/model/Field.java
+++ src/com/sibirbear/study/model/Field.java
@@ -1,21 +1,47 @@
 package com.sibirbear.study.model;
 
+import com.sibirbear.study.model.exceptions.InvalidPointException;
+import com.sibirbear.study.model.exceptions.PointAlreadyOccupiedException;
+
 import java.awt.*;
 
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+    public static final int MIN_COORDINATE = 0;
+    public static final int MAX_COORDINATE = FIELD_SIZE;
+
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize() {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if(checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws
+                                            InvalidPointException,
+                                            PointAlreadyOccupiedException {
+        if(checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+        if(field[point.x][point.y] != null) {
+            throw new PointAlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
     }
+
+    private boolean checkPoint(Point point) {
+        return checkCoordinate(point.x) || checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate (final int coordinate){
+        return coordinate < MIN_COORDINATE || coordinate >= MAX_COORDINATE;
+    }
+
 }
