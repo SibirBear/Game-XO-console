4b54e5fcb6578478f57ea26d581aa0f9d81154cc
SibirBear <test@test.com>
2020-03-28 23:12:29 +0700

    created class MoveController

---
+++ src/com/sibirbear/study/controllers/MoveController.java
@@ -0,0 +1,25 @@
+package com.sibirbear.study.controllers;
+
+import com.sibirbear.study.model.Field;
+import com.sibirbear.study.model.Figure;
+import com.sibirbear.study.model.exceptions.InvalidPointException;
+import com.sibirbear.study.model.exceptions.PointAlreadyOccupiedException;
+
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure(final Field field,
+                            final Figure figure,
+                            final Point point)
+            throws InvalidPointException,
+                   PointAlreadyOccupiedException {
+
+        if(field.getFigure(point) != null) {
+            throw new PointAlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
+
+    }
+
+}
