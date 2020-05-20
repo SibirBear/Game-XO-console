9381fffbfb505043948e13a6a0a4e94a8d0335ee
SibirBear <c.igor@list.ru>
2020-05-20 23:01:07 +0700

    added input players names

--- src/com/sibirbear/study/XOCLI.java
+++ src/com/sibirbear/study/XOCLI.java
@@ -7,21 +7,23 @@ import com.sibirbear.study.model.Player;
 import com.sibirbear.study.model.exceptions.InvalidPointException;
 import com.sibirbear.study.view.ConsoleView;
 
+import java.util.Scanner;
+
 public class XOCLI {
 
     public static void main(final String[] args) {
-        final String name1 = "Bear";
-        final String name2 = "Rabbit";
+        final String name1 = playerNameInput(1);
+        final String name2 = playerNameInput(2);
 
         final Player[] players = new Player[2];
         players[0] = new Player(name1, Figure.X);
         players[1] = new Player(name2, Figure.O);
 
-        final Game<Figure> gameXO =  new Game<>(players, new Field<>(3), "XO");
+        final Game<Figure> gameXO = new Game<>(players, new Field<>(3), "XO");
 
         final ConsoleView consoleView = new ConsoleView();
         consoleView.show(gameXO);
-        while(true) {
+        while (true) {
             try {
                 if (!consoleView.move(gameXO)) break;
             } catch (InvalidPointException e) {
@@ -30,5 +32,12 @@ public class XOCLI {
             consoleView.show(gameXO);
         }
     }
+
+        static String playerNameInput(final int count) {
+            Scanner sc = new Scanner(System.in);
+            System.out.format("Enter Player %s name: ", count);
+            return sc.nextLine();
+        }
+
 
 }
